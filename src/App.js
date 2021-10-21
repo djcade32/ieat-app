7e35eb52769f5a7eb85bb3370f3f055efc9067e4
Norman Cade <norman_cade32@yahoo.com>
2021-10-21 13:22:50 -0400

    working on visited places section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <p className="header-title-top">Discover the</p>
+        <h1 className="header-title-bottom">beauty of the world</h1>
+        <div className="input-wrapper">
+          <i className="fas fa-search search-icon"></i>
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search places"
+          />
+        </div>
+      </div>
+      <div className="visited-places-container">
+        <p className="visited-title-header">Visited</p>
+        <div class="place-container">
+          <h2 className="place-name">Tate Bakery & Cafe</h2>
+          <p className="place-location">Washington, DC</p>
+          <div className="place-rating">
+            <p className="rating-number">4.5</p>
+            <i className="fas fa-star star"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
